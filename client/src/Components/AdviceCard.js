0b6233a64dc65c20034e85a891fae1f77f52337d
Andrew Sanchez <druu831@gmail.com>
2023-03-07 15:16:21 -0800

    AdviceCard loading states and init network request

--- client/src/Components/AdviceCard.js
+++ client/src/Components/AdviceCard.js
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
+
 export default function AdviceCard() {
+  const [advice, setAdvice] = useState(null);
+
+  useEffect(() => {
+    console.log("before");
+
+    const fetchData = async () => {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const { slip } = await response.json();
+      setAdvice(slip);
+    };
+
+    fetchData();
+  }, [setAdvice]);
+
+  console.log(advice);
+
   return (
     <div className='card'>
-      <h5>Advice #117</h5>
-      <h4>
-        "It is easy to sit up and take notice, what's difficult is getting up
-        and taking action."
-      </h4>
-      <img src='/images/pattern-divider-desktop.svg' />
-      <button className='dice-btn'>
-        <img className='dice' src='images/icon-dice.svg' />
-      </button>
+      {advice === null ? (
+        <h5>Loading...</h5>
+      ) : (
+        <>
+          <h5>advice #{advice.id}</h5>
+          <h4>{advice.advice}</h4>
+          <img src='/images/pattern-divider-desktop.svg' />
+          <button className='dice-btn'>
+            <img className='dice' src='images/icon-dice.svg' />
+          </button>
+        </>
+      )}
     </div>
   );
 }
